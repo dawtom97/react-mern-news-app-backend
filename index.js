743d27977424e2f14d3e019c214e7ee52380a65c
Dawid <daw.tomczyk1997@gmail.com>
2022-06-30 16:56:42 +0200

    host and port fix v6

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from 'cors';
-import postRoutes from './routes/posts.js';
-import userRoutes from './routes/users.js'
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 const app = express();
 
-
-app.use(bodyParser.json({limit: "30mb", extended:true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
-app.use('/user', userRoutes);
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
-app.get('/',(req,res) => {
-    res.send('Hello to Daily News API')
-})
+app.get("/", (req, res) => {
+  res.send("Hello to Daily News API");
+});
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>app.listen(PORT, ()=>console.log("działa")))
-.catch((err) => console.log(err))
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(process.env.PORT, () => console.log("działa")))
+  .catch((err) => console.log(err));
 
 //mongoose.set('useFindAndModify',false);
